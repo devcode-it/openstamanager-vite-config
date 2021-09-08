9aac839f1f53e709b4fe2b3f9b3c4747b0cfff05
Maicol Battistini <maicolbattistini@live.it>
2021-09-08 16:10:58 +0200

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,13 @@
+import { UserConfig } from "vite";
+/**
+ * Returns the merged config
+ *
+ * @param config Config to merge with the default common one
+ */
+export default function osmConfig(config: UserConfig): Object;
+/**
+ * Returns the vite config object
+ *
+ * @param config Config to merge with the default common one
+ */
+export declare function defineConfig(config: UserConfig): import("vite").UserConfigExport;
---
+++ index.ts
@@ -0,0 +1,78 @@
+// noinspection JSUnusedGlobalSymbols
+
+import {UserConfig, defineConfig as viteConfig} from "vite";
+import {flowPlugin, esbuildFlowPlugin} from '@bunchtogether/vite-plugin-flow';
+
+function merge(obj1: Object, obj2: Object) {
+    for (const key in obj2) {
+        if (obj2.hasOwnProperty(key)) {
+            if (typeof obj1[key] === "object" && typeof obj2[key] === "object") {
+                obj1[key].merge(obj2[key]);
+
+                continue;
+            }
+
+            obj1[key] = obj2[key];
+        }
+    }
+
+    return obj1;
+}
+
+const osm_config: UserConfig = {
+    assetsInclude: ['js', 'png'],
+    build: {
+        minify: false,
+        rollupOptions: {
+            manualChunks: {},
+            output: {
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]'
+            },
+            preserveEntrySignatures: 'allow-extension'
+        }
+    },
+    esbuild: {
+        jsxFactory: 'm',
+        jsxFragment: 'm.Fragment'
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [esbuildFlowPlugin(/\.(flow|jsx?)$/, path => (/\.jsx$/.test(path) ? 'jsx' : 'js'), {
+                all: true,
+                pretty: true,
+                ignoreUninitializedFields: false
+            })]
+        }
+    },
+    plugins: [
+        flowPlugin({
+            include: /\.(flow|jsx?)$/,
+            exclude: /node_modules/,
+            flow: {
+                all: true,
+                pretty: true,
+                ignoreUninitializedFields: false
+            }
+        })
+    ]
+}
+
+/**
+ * Returns the merged config
+ *
+ * @param config Config to merge with the default common one
+ */
+export default function osmConfig(config: UserConfig) {
+    return merge(osm_config, config);
+}
+
+/**
+ * Returns the vite config object
+ *
+ * @param config Config to merge with the default common one
+ */
+export function defineConfig(config: UserConfig) {
+    return viteConfig(osmConfig(config));
+}
