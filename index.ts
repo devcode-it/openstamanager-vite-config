819ac61e3ad6ef6e22d5b09b411c6f935fa3b1a1
Maicol Battistini <maicolbattistini@live.it>
2022-10-28 21:03:19 +0200

    chore: Usa deepmerge

--- index.ts
+++ index.ts
@@ -2,37 +2,21 @@
 
 import autoprefixer from "autoprefixer";
 import {defineConfig as viteConfig, UserConfigExport} from "vite";
-
-function merge<T extends Object>(obj1: T, obj2: T) {
-    for (const key in obj2) {
-        if (obj2.hasOwnProperty(key)) {
-            if (typeof obj1[key] === "object" && typeof obj2[key] === "object") {
-                merge(obj1[key], obj2[key]);
-
-                continue;
-            }
-
-            obj1[key] = obj2[key];
-        }
-    }
-
-    return obj1;
-}
+import {deepmerge} from "deepmerge-ts";
 
 const osm_config: UserConfigExport = {
     assetsInclude: ['js', 'jpg', 'png', 'webp', 'txt'],
     build: {
         target: 'esnext',
-        minify: false,
-        rollupOptions: {
-            output: {
-                // manualChunks: {},
-                entryFileNames: '[name].js',
-                chunkFileNames: '[name].js',
-                assetFileNames: '[name].[ext]'
-            },
-            preserveEntrySignatures: 'allow-extension'
-        }
+        // rollupOptions: {
+        //     // output: {
+        //     //     // manualChunks: {},
+        //     //     entryFileNames: '[name].js',
+        //     //     chunkFileNames: '[name].js',
+        //     //     assetFileNames: '[name].[ext]'
+        //     // },
+        //     preserveEntrySignatures: 'allow-extension'
+        // }
     },
     css: {
         postcss: {
@@ -51,7 +35,7 @@ const osm_config: UserConfigExport = {
  * @param config Config to merge with the default common one
  */
 export default function osmConfig(config: UserConfigExport = {}) {
-    return merge(osm_config, config);
+    return deepmerge(osm_config, config);
 }
 
 /**
